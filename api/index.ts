bc1f6314de5c9605bd71088a3292189c01012746
arbinger <thomas.iniguez+github@free.fr>
2025-02-16 19:40:29 +0100

    patch

---
+++ api/index.ts
@@ -0,0 +1,48 @@
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+//const { sql } = require('@vercel/postgres');
+
+const bodyParser = require('body-parser');
+const path = require('path');
+
+// Create application/x-www-form-urlencoded parser
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+	res.sendFile(path.join(__dirname, '..', 'components', 'home.htm'));
+});
+
+app.get('/about', function (req, res) {
+	res.sendFile(path.join(__dirname, '..', 'components', 'about.htm'));
+});
+
+app.get('/uploadUser', function (req, res) {
+	res.sendFile(path.join(__dirname, '..', 'components', 'user_upload_form.htm'));
+});
+
+app.post('/uploadSuccessful', urlencodedParser, async (req, res) => {
+	try {
+		
+		res.status(200).send('<h1>User added successfully</h1>');
+	} catch (error) {
+		console.error(error);
+		res.status(500).send('Error adding user');
+	}
+});
+
+app.get('/allUsers', async (req, res) => {
+	try {
+		
+	} catch (error) {
+		console.error(error);
+		res.status(500).send('Error retrieving users');
+	}
+});
+
+app.listen(3000, () => console.log('Server ready on port 3000.'));
+
+module.exports = app;
